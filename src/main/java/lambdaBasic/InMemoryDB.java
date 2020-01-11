28b76bc67eb050e738e77be658fbde7eeea071aa
MikiKru <michal_kruczkowski@o2.pl>
2020-01-11 10:05:38 +0100

    lambda v2

--- src/main/java/lambdaBasic/InMemoryDB.java
+++ src/main/java/lambdaBasic/InMemoryDB.java
@@ -10,7 +10,14 @@ public interface InMemoryDB {
     List<Project> projects = new ArrayList<>(
             Arrays.asList(
                     new Project("SmartCity", 1_500_000, 15, 85,
-                            new ArrayList<>(Arrays.asList(Category.IT, Category.ENGINEERING, Category.TELECOMMUNICATION)))
+                    new ArrayList<>(Arrays.asList(Category.IT, Category.ENGINEERING, Category.TELECOMMUNICATION))),
+                    new Project("ThermoNuclearCar",40_000_000,250,85.00,Arrays.asList(Category.ENGINEERING)),
+                    new Project("Formula 1 Poslish Racing Team", 5_000_000, 25, 65, new ArrayList<>(Arrays.asList(Category.ENGINEERING))),
+                    new Project("BetterThanSmartCity", 1_200_00, 100, 90, new ArrayList<>(Arrays.asList(Category.IT))),
+                    new Project("CleanAir", 4_000_000, 25,95, new ArrayList<>(Arrays.asList(Category.EDUCATION))),
+                    new Project("DebtCollection",2_000_000, 6, 100, new ArrayList<>(Arrays.asList(Category.FINANCE, Category.IT))),
+                    new Project ("EgzoSkeleton", 6000000, 50, 50,new ArrayList<>(Arrays.asList(Category.ENGINEERING, Category.ELECTRONICS, Category.IT))),
+                    new Project ("Java", 20000,20,50, new ArrayList<>(Arrays.asList(Category.IT))
             )
     );
     // metody statyczne
